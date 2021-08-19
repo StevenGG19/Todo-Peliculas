7536d7946ee14a8a54840370b0774f1778f092f9
StevenGG19 <stevengg39@gmail.com>
2021-08-19 00:20:24 -0500

    creation of a dialog

--- app/src/main/java/com/steven/todopeliculas/ui/moviedetails/MovieDetailFragment.kt
+++ app/src/main/java/com/steven/todopeliculas/ui/moviedetails/MovieDetailFragment.kt
@@ -5,16 +5,23 @@ import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.steven.todopeliculas.R
 import com.steven.todopeliculas.application.AppConstants
 import com.steven.todopeliculas.databinding.FragmentMovieDetailBinding
 
-class MovieDetailFragment : Fragment() {
+class MovieDetailFragment : DialogFragment() {
     private var _binding: FragmentMovieDetailBinding? = null
     private val binding get() = _binding!!
     private val args by navArgs<MovieDetailFragmentArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.Theme_TodoPeliculas_light)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +34,12 @@ class MovieDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbarMovieDetail.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_arrow_back)
+        binding.toolbarMovieDetail.setNavigationOnClickListener{
+            dismiss()
+        }
+
         Glide.with(requireContext()).load(AppConstants.IMAGE_URL + args.posterImageUrl)
             .centerCrop().into(binding.imgMovie)
         Glide.with(requireContext()).load(AppConstants.IMAGE_URL + args.backgroundImageUrl)
